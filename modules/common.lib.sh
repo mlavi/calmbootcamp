1a90d3f529fbaa723866926152bc7bd0d5ba7e83
Mark Lavi <mlavi@users.noreply.github.com>
2020-06-15 18:33:24 -0500

    udacity/course/build_lesson.sh: Course 2, Lesson 0

--- modules/common.lib.sh
+++ modules/common.lib.sh
@@ -1,9 +1,38 @@
 #!/usr/bin/env bash
 # derived from https://github.com/mlavi/stageworkshop/blob/master/scripts/lib.common.sh
 
+function finish() {
+  log "${0}|${SECONDS}|executed in ${SECONDS} seconds."
+  echo
+}
+
+function html_footer() {
+  # REQUIRED: global variable ${FILESPEC}
+  cat << EoM >> ${FILESPEC}
+  </ol>
+</body>
+</html>
+EoM
+}
+
+function html_header() {
+  # REQUIRED: global variable ${FILESPEC}, ${COURSE}, ${LESSON}
+  cat << EoM > ${FILESPEC}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Course ${COURSE}.sheet:L${LESSON}.tab:\$F1.cell</title>
+</head>
+<body>
+  <ol>
+EoM
+}
+
 function launch() {
   local _release
 
+  # OPTIONAL: global variable ${RELEASE}
   if [[ -e ${RELEASE} ]]; then
     _release="|release: $(grep FullSemVer ${RELEASE} | awk -F\" '{print $4}')"
   fi
@@ -11,26 +40,23 @@ function launch() {
   log "$(basename ${0})${_release}|=============="
 }
 
-function finish() {
-  log "${0}|${SECONDS}|executed in ${SECONDS} seconds."
-  echo
-}
-
 function log() {
   local _caller
   local _hold
-  local _logfile
-
-  if ((${#LOGFILE} > 5)); then
-    _logfile=${LOGFILE}
-  else
-    _logfile=$(basename $(git rev-parse --show-toplevel)).log
-  fi
 
   _caller=$(echo -n "$(caller 0 | awk '{print $2}')")
   _hold="$(date '+%Y-%m-%d %H:%M:%S')|$$|${_caller}|${1}"
 
+  # OPTIONAL: global variable ${LOGFILE}
   if [[ -n ${LOGFILE} ]]; then
+    local _logfile
+
+    if ((${#LOGFILE} > 5)); then
+      _logfile=${LOGFILE}
+    else
+      _logfile=$(basename $(git rev-parse --show-toplevel)).log
+    fi
+
     echo ${_hold} >> ${_logfile}
   fi
 
